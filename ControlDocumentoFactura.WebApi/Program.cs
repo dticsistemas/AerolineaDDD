8511d4ef16ae84fd0bb9854ebae534302c98a0f9
103296617+dticsistemas@users.noreply.github.com <103296617+dticsistemas@users.noreply.github.com>
2022-07-28 22:43:15 -0400

    update

--- ControlDocumentoFactura.WebApi/Program.cs
+++ ControlDocumentoFactura.WebApi/Program.cs
@@ -13,12 +13,11 @@ namespace ControlDocumentoFactura.WebApi {
 			CreateHostBuilder(args).Build().Run();
 		}
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    
-                });
-    }
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+				Host.CreateDefaultBuilder(args)
+					.ConfigureWebHostDefaults(webBuilder => {
+						webBuilder.UseStartup<Startup>();
+
+					});
+	}
 }
